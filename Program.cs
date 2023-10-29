79010efbe02d7f2f54c03028ca5c6e4ab9eccc48
Hitalo Jacome <jacomehitalo@gmail.com>
2023-10-29 15:51:03 -0300

    Continuação

--- Program.cs
+++ Program.cs
@@ -4,7 +4,20 @@ namespace ScreamSound {
     class Program {
         static void Main(string[] args) {
 
-            
+            Console.Write("Qual seu nome? ");
+            string name = Console.ReadLine();
+            Console.Write("Qual sua idade? ");
+            int age = int.Parse(Console.ReadLine());
+            Console.Write("Qual seu curso na faculdade? ");
+            string course = Console.ReadLine();
+
+            Console.WriteLine($"Olá {name}! Vi que você tem {age} anos e atualmente é estudante de {course}");
+
+            void Welcome() {
+                Console.WriteLine("Olá mundo!");
+            }
+
+            Welcome();
 
         }
     }
